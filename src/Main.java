696b8539f894a9ddb8acf48ac11dbf4581ecd3ba
Unknown <dwiprasetyo.an@gmail.com>
2018-12-15 23:57:01 +0100

    Add basic MergeSort implementation

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,47 @@
+import sort.MergeSort;
+import utils.*;
 import benchmark.BenchmarkRunner;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        BasicInputStream basicInputStream = new BasicInputStream();
+        BasicOutputStream basicOutputStream = new BasicOutputStream();
+
+        MergeSort mergeSort = new MergeSort(basicInputStream, basicOutputStream);
+
+        try {
+            // Creating sample data to sort, assume that data/ directory is created
+            basicOutputStream.create("data/input.dat");
+            basicOutputStream.write(42);
+            basicOutputStream.write(149);
+            basicOutputStream.write(21);
+            basicOutputStream.write(54);
+            basicOutputStream.write(642);
+            basicOutputStream.write(232);
+            basicOutputStream.write(59);
+            basicOutputStream.write(80);
+            basicOutputStream.write(77);
+            basicOutputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mergeSort.doSort("data/input.dat", 2);
+            mergeSort.doMerge(2);
+
+            basicInputStream.open("data/result.dat");
+            while (!basicInputStream.endOfStream()) {
+                System.out.println(basicInputStream.readNext());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         BenchmarkRunner runner = new BenchmarkRunner("input.csv");
         runner.run();
     }
---
+++ src/sort/MergeSort.java
@@ -0,0 +1,112 @@
+package sort;
+
+import utils.BaseInputStream;
+import utils.BaseOutputStream;
+
+import java.io.File;
+import java.util.*;
+
+import static java.util.Arrays.sort;
+
+/*
+    Both availableMemory and fileLength is measured in the number of 32-bit integer.
+ */
+public class MergeSort {
+    BaseInputStream inputStream;
+    BaseOutputStream outputStream;
+
+    private String TMPDIR = "data/tmp/";
+    private String DATADIR = "data/";
+
+    public MergeSort(BaseInputStream inputStream, BaseOutputStream outputStream) {
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+
+
+        // write the sorted array to temporary file
+        File dataDir = new File(this.DATADIR);
+        if (!dataDir.exists()) {
+            dataDir.mkdir();
+        }
+        // write the sorted array to temporary file
+        File tmpDir = new File(this.TMPDIR);
+        if (!tmpDir.exists()) {
+            tmpDir.mkdir();
+        }
+    }
+
+    public void doSort(String filePath, int availableMemory) throws Exception {
+        File inputFile = new File(filePath);
+
+        // WARNING: This assume that each int consists of 4 bytes (hence, it's a 32-bit integers
+        long fileLength = inputFile.length() / 4;
+
+        // open stream
+        inputStream.open(filePath);
+        int position = 0;
+
+        // TODO: Parallelize the internal memory sorting process to as many as filePath/availableMemory
+        while (position < fileLength) {
+            // read content of file as int
+            ArrayList<Integer> bucket = new ArrayList<>();
+            for (int i = 0; i < availableMemory && !inputStream.endOfStream(); i++) {
+                int toBeAddedToBucket = inputStream.readNext();
+                bucket.add(toBeAddedToBucket);
+            }
+
+            // sort the array
+            sort(bucket.toArray());
+
+            outputStream.create(this.TMPDIR.concat(String.valueOf(java.util.UUID.randomUUID())).concat(".dat"));
+            // write the output to intermediate file
+            for (int i = 0; i < availableMemory & i < bucket.size(); i++) {
+                outputStream.write(bucket.get(i));
+            }
+            position += availableMemory;
+        }
+        outputStream.close();
+    }
+
+    public void doMerge(int numberOfStreamToMerge) throws Exception {
+        // list all intermediate files
+        File dir = new File(this.TMPDIR);
+        File[] listOfFiles = dir.listFiles();
+
+        // open an outputstream
+        this.outputStream.create(this.DATADIR.concat("result.dat"));
+
+        // create a priority queue with size of numberOfStreamToMerge
+        // pack the stream with it's readNext() value
+        PriorityQueue<Map.Entry<BaseInputStream, Integer>> pq = new PriorityQueue<>(numberOfStreamToMerge,
+                Comparator.comparingInt(Map.Entry::getValue));
+
+        // for all streams opened, add stream.readNext() to pq
+        assert listOfFiles != null;
+        for (int i = 0; i < listOfFiles.length; i++) {
+            BaseInputStream inputStreamToAdd = inputStream.getClass().newInstance();
+            inputStreamToAdd.open(listOfFiles[i].getPath());
+
+            int valToAdd = inputStreamToAdd.readNext();
+            pq.add(new AbstractMap.SimpleEntry<>(inputStreamToAdd, valToAdd));
+        }
+
+        // while pq is not empty
+        while (!pq.isEmpty()) {
+            // write pq.remove() to outputstream
+            Map.Entry<BaseInputStream, Integer> removedEntry = pq.peek();
+            outputStream.write(pq.remove().getValue());
+
+            // insert removed pq stream to pq
+            if (!removedEntry.getKey().endOfStream()) {
+                pq.add(new AbstractMap.SimpleEntry<>(removedEntry.getKey(), removedEntry.getKey().readNext()));
+            }
+        }
+        // close the outstream
+        outputStream.close();
+        // delete tempFiles
+        for (File tmpFile : listOfFiles) {
+            tmpFile.delete();
+        }
+
+    }
+}
